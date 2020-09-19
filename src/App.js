ad055df5e03da9f3ae5047209a8d07e1b99b0699
chamodya wimansha <chamodyawimansha@gmail.com>
2020-09-19 16:41:33 +0530

    basic html struture created with buttons

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: "",
+    };
+  }
+
+  render() {
+    return (
+      <div id="calculator">
+        <div id="display-container">
+          <div id="display">{this.state.display}</div>
+        </div>
+        <div id="key-container">
+          <div id="operation-keys-top">
+            <button id="ac" className="key">
+              AC
+            </button>
+            <button id="division" className="key">
+              /
+            </button>
+            <button id="multiple" className="key">
+              X
+            </button>
+          </div>
+          <div id="number-keys">
+            <button id="seven" className="key">
+              7
+            </button>
+            <button id="eight" className="key">
+              8
+            </button>
+            <button id="nine" className="key">
+              9
+            </button>
+            <button id="four" className="key">
+              4
+            </button>
+            <button id="five" className="key">
+              5
+            </button>
+            <button id="six" className="key">
+              6
+            </button>
+            <button id="one" className="key">
+              1
+            </button>
+            <button id="two" className="key">
+              2
+            </button>
+            <button id="three" className="key">
+              3
+            </button>
+            <button id="zero" className="key">
+              0
+            </button>
+            <button id="decimal" className="key">
+              .
+            </button>
+          </div>
+          <div id="operation-keys-right">
+            <button id="subtraction" className="key">
+              -
+            </button>
+            <button id="addition" className="key">
+              +
+            </button>
+            <button id="equel" className="key">
+              =
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+// metrix green display, navly blue body, ash buttons
